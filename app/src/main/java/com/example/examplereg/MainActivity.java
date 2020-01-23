5172d5f6264f56e2e7152292f1258e352b2387d5
Denis <la2aang1337@yandex.ru>
2020-01-23 05:37:19 +0300

    Add DB

--- app/src/main/java/com/example/examplereg/MainActivity.java
+++ app/src/main/java/com/example/examplereg/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -17,15 +18,31 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.GenericTypeIndicator;
+import com.google.firebase.database.ValueEventListener;
 import com.vk.sdk.VKAccessToken;
 import com.vk.sdk.VKAccessTokenTracker;
 import com.vk.sdk.VKCallback;
 import com.vk.sdk.VKScope;
 import com.vk.sdk.VKSdk;
+import com.vk.sdk.api.VKApi;
+import com.vk.sdk.api.VKApiConst;
 import com.vk.sdk.api.VKError;
+import com.vk.sdk.api.VKParameters;
+import com.vk.sdk.api.VKRequest;
+import com.vk.sdk.api.VKResponse;
 import com.vk.sdk.util.VKUtil;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -34,6 +51,8 @@ public class MainActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
 
+    private DatabaseReference myRef;
+    private HashMap<String,String> DiscrTasks;
     private String[] scope = new String[]{VKScope.EMAIL};
 
     Button registration;
@@ -43,11 +62,49 @@ public class MainActivity extends AppCompatActivity {
     EditText log;
     EditText pass;
     TextView label;
+
+
+
+    public void updateUI()
+    {
+        myRef = FirebaseDatabase.getInstance().getReference();
+        FirebaseUser user = mAuth.getCurrentUser();
+        String str= user.getUid().toString();
+
+       /* FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("UvbprWzQtohKYAUjCllGsUt9OeB2");
+
+        //myRef.setValue("Hello, World!");*/
+        myRef.child(str).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange( DataSnapshot dataSnapshot) {
+                GenericTypeIndicator<HashMap<String,String>> t = new GenericTypeIndicator<HashMap<String,String>>() {};
+                DiscrTasks = dataSnapshot.getValue(t);
+                Intent MainActivity2 = new Intent(MainActivity.this, MainActivity_Navigation.class);
+                MainActivity2.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                MainActivity2.putExtra("first_name", ""+DiscrTasks.get("first_name").toString());
+                MainActivity2.putExtra("second_name", ""+DiscrTasks.get("second_name").toString());
+                MainActivity2.putExtra("Flag", "mAuth");
+
+                startActivity(MainActivity2);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myRef = FirebaseDatabase.getInstance().getReference();
+
 
         mAuth = FirebaseAuth.getInstance();
 
@@ -79,6 +136,9 @@ public class MainActivity extends AppCompatActivity {
 
             }
         };
+
+
+
         //клик регистрации
         View.OnClickListener clickregistration = new View.OnClickListener()
     {
@@ -93,7 +153,14 @@ public class MainActivity extends AppCompatActivity {
                         {
                             if(task.isSuccessful())
                             {
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                String str= user.getUid().toString();
 
+                                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                                DatabaseReference myRef = database.getReference(str);
+
+                                myRef.child("first_name").setValue("InsertName");
+                                myRef.child("second_name").setValue("InsertName");
                                 Toast.makeText(MainActivity.this, "Регистрация успешна", Toast.LENGTH_SHORT).show();
 
                             }
@@ -115,10 +182,10 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if(task.isSuccessful()) {
-                            Intent MainActivity2 = new Intent(MainActivity.this, MainActivity2.class);
-                            MainActivity2.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                            startActivity(MainActivity2);
-                            Toast.makeText(MainActivity.this, "Aвторизация успешна", Toast.LENGTH_SHORT).show();
+
+                            updateUI();
+                            Toast.makeText( MainActivity.this, "Aвторизация успешна", Toast.LENGTH_SHORT).show();
+
                         }else
                             Toast.makeText(MainActivity.this, "Aвторизация провалена", Toast.LENGTH_SHORT).show();
 
@@ -145,17 +212,65 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
-
+    private VKAccessToken AccessTOKEN;
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (!VKSdk.onActivityResult(requestCode, resultCode, data, new VKCallback<VKAccessToken>() {
             @Override
             public void onResult(VKAccessToken res) {
 // Пользователь успешно авторизовался
-                Intent MainActivity2 = new Intent(MainActivity.this, MainActivity2.class);
-                startActivity(MainActivity2);
-                Toast.makeText(MainActivity.this, "Aвторизация успешна", Toast.LENGTH_SHORT).show();
 
+                Toast.makeText(MainActivity.this, "Aвторизация успешна", Toast.LENGTH_SHORT).show();
+                AccessTOKEN =res;
+                VKRequest request = VKApi.users().get(VKParameters.from(VKApiConst.FIELDS,"first_name, last_name"));
+                request.executeWithListener(new VKRequest.VKRequestListener() {
+                    @Override
+                    public void onComplete(VKResponse response) {
+                        JSONObject jObject=new JSONObject();
+                        String firstname="InsertName",secondname="InsertName",id="nul";
+                        try {
+
+                             jObject = new JSONObject(response.responseString);
+                             JSONArray jsonArray = jObject.getJSONArray("response");
+                            for (int i = 0; i < jsonArray.length(); i++) {
+                                JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                                firstname = jsonObject.getString("first_name");
+                                secondname = jsonObject.getString("last_name");
+                                id = jsonObject.getString("id");
+
+                            }
+                            FirebaseDatabase database = FirebaseDatabase.getInstance();
+                            String var = AccessTOKEN.accessToken.toString();
+                            DatabaseReference myRef = database.getReference(id);
+
+                            myRef.child("first_name").setValue(firstname);
+                            myRef.child("second_name").setValue(secondname);
+
+                            Intent MainActivity2 = new Intent(MainActivity.this, MainActivity_Navigation.class);
+                            MainActivity2.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                            MainActivity2.putExtra("first_name", ""+firstname);
+                            MainActivity2.putExtra("second_name", ""+secondname);
+                            MainActivity2.putExtra("id", id);
+                            MainActivity2.putExtra("Flag", "VK");
+
+                            startActivity(MainActivity2);
+                        } catch (JSONException e) {
+                            Toast.makeText(MainActivity.this, "Ошибка получения имени фамилии", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                    @Override
+                    public void onError(VKError error) {
+//Do error stuff
+                    }
+                    @Override
+                    public void attemptFailed(VKRequest request, int attemptNumber, int totalAttempts) {
+//I don't really believe in progress
+                    }
+                });
+
+String var="22";
             }
             @Override
             public void onError(VKError error) {
---
+++ app/src/main/java/com/example/examplereg/ui/slideshow/SlideshowFragment.java
@@ -0,0 +1,91 @@
+package com.example.examplereg.ui.slideshow;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+
+import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+
+import com.example.examplereg.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import org.w3c.dom.Text;
+
+public class SlideshowFragment extends Fragment {
+
+    private SlideshowViewModel slideshowViewModel;
+    Button SaveButton;
+    TextView text_slideshow;
+    EditText first_name, second_name;
+
+    String Flag="nul",id="nul";
+    private FirebaseAuth mAuth;
+    private DatabaseReference myRef;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        slideshowViewModel =
+                ViewModelProviders.of(this).get(SlideshowViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_slideshow, container, false);
+
+
+        first_name = (EditText) root.findViewById(R.id.editText);
+        second_name = (EditText)  root.findViewById(R.id.editText2);
+        SaveButton = (Button) root.findViewById(R.id.button);
+
+        Intent intent = getActivity().getIntent();
+        first_name.setText(intent.getStringExtra("first_name"));
+        second_name.setText(intent.getStringExtra("second_name"));
+        Flag= intent.getStringExtra("Flag");
+        id= intent.getStringExtra("id");
+        mAuth = FirebaseAuth.getInstance();
+
+
+
+        SaveButton.setOnClickListener(clickSaveButton);
+
+        return root;
+
+    }
+
+    //клик сохранения
+    View.OnClickListener clickSaveButton = new View.OnClickListener()
+    {
+
+        @Override
+        public void onClick(View v)
+        {
+            String str;
+            if (Flag.equals("mAuth")) {
+                FirebaseUser user = mAuth.getCurrentUser();
+                str = user.getUid();
+            }
+            else
+            {
+                str=id;
+            }
+            FirebaseDatabase database = FirebaseDatabase.getInstance();
+            DatabaseReference myRef = database.getReference(str);
+
+            myRef.child("first_name").setValue(first_name.getText().toString());
+            myRef.child("second_name").setValue(second_name.getText().toString());
+
+        }
+    };
+}
